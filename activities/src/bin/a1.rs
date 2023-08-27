f76d560dda0a1c2b8ae2b983dc91c8b2e44f2dc5
manavkdubey <dmanavkumar24@gmail.com>
2023-08-28 02:06:29 +0530

    Initial commit

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,18 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name()->&'static str{
+    let my_first_name="Manav";
+    my_first_name
+}
+
+fn last_name()->&'static str{
+    let my_last_name="Kumar Dubey";
+    my_last_name
+}
+
+
+fn main(){
+    println!("{} {}",first_name(),last_name());
+
+}
